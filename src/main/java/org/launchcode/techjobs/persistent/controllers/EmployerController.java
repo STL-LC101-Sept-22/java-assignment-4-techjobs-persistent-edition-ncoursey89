e15cabd061bfe169b1ba325f4ecfce57de0334ec
ncoursey89 <nathanael.coursey@gmail.com>
2023-03-22 18:58:34 -0500

    added sections 1&2

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -9,17 +11,23 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.Optional;
 
+
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
-
-
+    @Autowired
+    private EmployerRepository employerRepository;
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("name", employerRepository.findAll());
+       return "employers/index";
+    }
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
         model.addAttribute(new Employer());
         return "employers/add";
     }
-
+// Contollers #3
     @PostMapping("add")
     public String processAddEmployerForm(@ModelAttribute @Valid Employer newEmployer,
                                     Errors errors, Model model) {
@@ -27,14 +35,15 @@ public class EmployerController {
         if (errors.hasErrors()) {
             return "employers/add";
         }
-
+        //Controllers#3
+        employerRepository.save(newEmployer);
         return "redirect:";
     }
-
+//Controllers #4 - displayViewEmployer will be in charge of rendering a page to view the contents of an individual employer object. It will make use of the employer object's id field to grab the correct information from employerRepository. optEmployer is currently initialized to null.
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional optEmployer = employerRepository.findById(employerId);//null;
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location is required")
+    @Size(min = 3, max = 500)
+    private String location;
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public Employer() {}
 }
